047f1882c6116507d86159c07b8aa6765487742d
eshao98 <eshao98@stanford.edu>
2019-05-07 00:11:00 -0700

    Change transition interval time.

--- jscode.js
+++ jscode.js
@@ -1,37 +1,43 @@
-      
-      var link = document.getElementById("link");
-      var hover_img=document.getElementById("img");
+    // Full JavaScript Code
 
-      document.addEventListener("mousemove", getMouse);
+	var link = document.getElementById("link");
+	var hover = document.getElementById("img");
+	// Setting up variables to hold positions
+	var position = {x:0, y:0};
+	var mouse = {x:0, y:0};
 
-      var img_pos={x:0, y:0};
+	document.addEventListener("mousemove", getMousePosition);
+	
+	// Event handling functions
 
-      setInterval(followMouse, 10);
+	// Takes in event e parameter; mouse movement is tracked
+	function getMousePosition(e){
+		mouse.x = e.pageX;
+		mouse.y = e.pageY;
+	}
 
-      var mouse={x:0, y:0};
+	// Image position on hover
+	function trailMousePosition(){
+		// Setting up variables 
+		var distanceX = mouse.x - position.x;
+		var distanceY = mouse.y - position.y;
+		position.x+=distanceX/10;
+		position.y+=distanceY/10;
 
-      function getMouse(elem){
-      	mouse.x = elem.pageX;
-      	mouse.y = elem.pageY;
-      }
+		// Manages how the image is positioned relative to mouse hover
+		hover.style.left = position.x + 100 + "px";
+		hover.style.top = position.y + 100 + "px";
 
-      function followMouse(){
-      	var distX = mouse.x - img_pos.x;
-      	var distY = mouse.y - img_pos.y;
+		hover_image.setAttribute("class", "move")
+	}
 
-      	img_pos.x+=distX/10;
-      	img_pos.y+=distY/10;
+	// Smooth out the transition, making it slower, "floaty"
+	setInterval(trailMousePosition, 50);
 
-      	hover_img.style.left = img_pos.x + 100 + "px";
-      	hover_img.style.top = img_pos.y + 100 + "px";
-
-      	hover_image.setAttribute("class", "move")
-      }
-
-      link.onmouseover = function(){
-        img.style.display = "block";
-      }
-
-      link.onmouseout = function(){
-        img.style.display = "none";
-      }
+	// Manages basic hiding and displaying of the image on mouse hover
+	link.onmouseover = function(){
+	img.style.display = "block";
+	}
+	link.onmouseout = function(){
+	img.style.display = "none";
+	}
